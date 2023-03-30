a0fafd349422c5dfc49f7a5cd9b1ff145895f95d
Aysur Kuchkashev <aysursabin@gmail.com>
2023-03-30 13:33:24 +0700

    chane content newstory

--- src/components/NewsContent/NewsContent.jsx
+++ src/components/NewsContent/NewsContent.jsx
@@ -1,19 +1,27 @@
 import { Link } from 'react-router-dom';
+import Button from '../UI/Button';
+import unixTimeToDateTime from '../../utils/unixTimeToDateTime';
 
 const NewsContent = ({ data }) => {
-  const { url, text } = data;
+  const { url, text, title, by, time, descendants } = data;
 
   const content = text ? (
-    <div dangerouslySetInnerHTML={{ __html: text }} />
+    <p dangerouslySetInnerHTML={{ __html: text }} />
   ) : (
     <a href={url} rel="noopener noreferrer" target="_blank">Link News</a>
   );
 
   return (
     <div>
+      <h2>{title}</h2>
+      <div>
+        <div>Author: {by}</div>
+        <div>{unixTimeToDateTime(time)}</div>
+        <div>Comments count:{descendants}</div>
+      </div>
       {content}
       <Link to="../.." relative="path">
-        To All News
+        <Button>To All News</Button>
       </Link>
     </div>
   );
--- src/pages/NewsDetails.jsx
+++ src/pages/NewsDetails.jsx
@@ -1,7 +1,6 @@
 import { useParams } from 'react-router-dom';
 
 import Fetch from '../utils/Fetch';
-import NewsItem from '../components/NewsItem/NewsItem';
 import NewsContent from '../components/NewsContent/NewsContent';
 import Comments from '../components/Comments/Comments';
 
@@ -13,7 +12,6 @@ const NewsDetails = () => {
         url={`https://hacker-news.firebaseio.com/v0/item/${params.newsId}.json`}
         renderSuccess={({ data }) => (
           <>
-            <NewsItem data={data} />
             <NewsContent data={data}/>
             <Comments data={data}/>
           </>
