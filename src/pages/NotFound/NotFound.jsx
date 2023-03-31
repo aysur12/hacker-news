f3ad3b7edf6917c7db3087e56d72df68960538c9
Aysur Kuchkashev <aysursabin@gmail.com>
2023-03-31 17:41:43 +0700

    fix text

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -7,7 +7,7 @@ const NotFound = () => {
   return (
     <section className={styles['not-found-page']}>
       <p className={styles['not-found-page__info']}>404</p>
-      <h2 className={styles['not-found-page__title']}>Ooops, page Not found</h2>
+      <h2 className={styles['not-found-page__title']}>Ooops, page not found</h2>
       <Link className={styles['not-found-page__link']} to="/hacker-news">
         <Button title="back to news list">
           <RiArrowGoBackLine />
