74358dbbe5d025d935be86091c63542401ff4a03
Aysur Kuchkashev <aysursabin@gmail.com>
2023-03-31 17:39:03 +0700

    add notFound page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,11 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import { RiArrowGoBackLine } from 'react-icons/ri';
+import Button from '../../components/UI/Button';
+import styles from './NotFound.module.scss';
 
 const NotFound = () => {
   return (
-    <section>
-      <p>
-        Page Not found. Go to <Link to="/">News</Link>
-      </p>
+    <section className={styles['not-found-page']}>
+      <p className={styles['not-found-page__info']}>404</p>
+      <h2 className={styles['not-found-page__title']}>Ooops, page Not found</h2>
+      <Link className={styles['not-found-page__link']} to="/hacker-news">
+        <Button title="back to news list">
+          <RiArrowGoBackLine />
+        </Button>
+      </Link>
     </section>
   );
 };
